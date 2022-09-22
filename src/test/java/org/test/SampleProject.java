a1ff3e9c7de2129f4a9726a0b108f63b0d7e0b38
krishkesha <krish.eee50@gmail.com>
2022-09-22 16:31:29 +0530

    keshav's Code

--- src/test/java/org/test/SampleProject.java
+++ src/test/java/org/test/SampleProject.java
@@ -10,7 +10,14 @@ public class SampleProject {
 	private void test1() {
 		
 		System.out.println("test1");
-
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
+		System.out.println("test5");
+	
+	
+	
 	}
+	
 	
 }
